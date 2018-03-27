2a22b6070e4f10bdca59bbba901acc1613a7c009
Mark Tice <marktice@protonmail.com>
2018-03-27 12:12:53 +1100

    Add comments to server

--- server.rb
+++ server.rb
@@ -1,7 +1,11 @@
 require 'webrick'
 require_relative 'routes'
 
+# create and mount a server
 server = WEBrick::HTTPServer.new(Port: 3000)
-trap 'INT' do server.shutdown end
+
+# add the router to the server
 server.mount '/', Router
+trap 'INT' do server.shutdown end
+
 server.start
